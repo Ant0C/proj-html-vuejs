59d684d67f5f2dd8cd68e57a7e0f1b749a85c865
Ant0C <cafeo97@gmail.com>
2023-03-13 12:59:55 +0100

    CREATE project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+
+export default {
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+}
+</script>
+
+<template>
+  <Header></Header>
+  <Main></Main>
+  <Footer></Footer>
+</template>
+
+<style lang="scss">
+@use '../src/style/general.scss' as *;
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+  data() {
+    return {
+      LinksNav: [
+
+      ]
+    }
+  },
+}
+</script>
+
+<template>
+  <nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-">
+
+        </div>
+        <div class="col">
+
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@use '../src/style/general.scss' as *;
+</style>
