b5b9ae528841c0d8f78328e3cd8a3900635322cc
Ant0C <cafeo97@gmail.com>
2023-03-13 16:25:59 +0100

    FEAT added navbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,28 @@
 export default {
   data() {
     return {
-      LinksNav: [
-
+      linksNav: [
+        {
+          link: 'HOME',
+        },
+        {
+          link: 'SHOP',
+        },
+        {
+          link: 'ABOUT',
+        },
+        {
+          link: 'GALLERY',
+        },
+        {
+          link: 'LOCATIONS',
+        },
+        {
+          link: 'JOURNAL',
+        },
+        {
+          link: 'CONTACT',
+        },
       ]
     }
   },
@@ -15,11 +35,19 @@ export default {
   <nav>
     <div class="container">
       <div class="row">
-        <div class="col-">
-
+        <div class="col-logo">
+          <img src="/img/avada-bakery-logo-retina-200x97.png" alt="logo">
         </div>
-        <div class="col">
-
+        <div class="col-menu">
+          <ul>
+            <li v-for="el in linksNav">
+              <a href="#">{{ el.link }}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-account">
+          MY ACCOUNT
+          <!-- <font-awesome-icon icon="fa-solid fa-cart-shopping" /> -->
         </div>
       </div>
     </div>
@@ -28,4 +56,51 @@ export default {
 
 <style lang="scss" scoped>
 @use '../src/style/general.scss' as *;
+
+nav {
+  background-color: #F3E7F1;
+}
+
+.row {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .col-logo,
+  .col-menu,
+  .col-account {
+    flex-basis: calc(100%/3);
+    font-family: 'Source Serif Pro', serif;
+  }
+
+  .col-logo {
+
+    img {
+      width: 160px;
+      padding: 1.25rem 0rem;
+      padding-left: 2.18rem;
+    }
+  }
+
+  .col-menu {
+    ul {
+      display: flex;
+      gap: 2.37rem;
+      color: #5b2ca2;
+
+      li {
+        padding: 0.4rem;
+        font-size: 0.75rem;
+      }
+    }
+  }
+
+  .col-account {
+    padding: 1.25rem 0rem;
+    padding-right: 2.18rem;
+    text-align: end;
+    color: #5b2ca2;
+    font-size: 0.75rem;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,3 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+
+// import { library } from '@fortawesome/fontawesome-svg-core'
+
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// import { facartshopping } from '@fortawesome/free-solid-svg-icons'
+
+// library.add(facartshopping)
+
+// createApp(App)
+
+//     .component('font-awesome-icon', FontAwesomeIcon)
+
+//     .mount('#app')
